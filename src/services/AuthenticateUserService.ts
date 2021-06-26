169f03c509e9cdda03782e04bc5caed5ec5fd567
Mahilson Pereira <m.hagner.sp@gmail.com>
2021-06-26 00:21:37 -0300

    Initial commit

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getCustomRepository } from "typeorm"
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UsersRepositories)
+
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const token = sign(
+            {
+                email: user.email
+            },
+            "0db94bd605376879ef509478c8f95ca7",
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        );
+
+        return token
+    }
+}
+
+export { AuthenticateUserService }
